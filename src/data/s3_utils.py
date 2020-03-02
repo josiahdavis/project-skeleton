097fd3ff4f1b2d167c8e8cd68b35f858e8ab29fa
Josiah Davis <josiah.j.davis@gmail.com>
2020-03-02 06:47:29 +0000

    documentation for s3 functions

--- src/data/s3_utils.py
+++ src/data/s3_utils.py
@@ -3,20 +3,55 @@ import os
 
 
 def make_dir_if_needed(local_dir):
+    """Creates a directory if doesn't already exist.
+
+    Args:
+        local_dir (str): a directory.
+
+    Returns:
+        Nothing except a imple print confirmation
+
+    """
     if not os.path.isdir(local_dir):
         os.mkdir(local_dir)
         print(f"Created {local_dir}")
 
 
 def download_objects(s3_client, bucket, s3_keys, local_dir, verbose=False):
+    """Downloads objects from s3 to a local directory.
+
+    Args:
+        s3_client (obj): a low-level client of AWS S3.
+        bucket (str): the bucket name.
+        s3_keys (list[str]): the path to the s3 object along with the object name.
+        local_dir (str): where the files should be downloaded toi.
+        verbose (bool): whether to print out files as they are being downloaded. 
+    
+    Returns:
+        List of local files and their location.
+    """
     make_dir_if_needed(local_dir)
+    local_keys = []
     for s3_key in s3_keys:
         file_name = s3_key.split("/")[-1]
         local_key = os.path.join(local_dir, file_name)
         download_object(s3_client, bucket, s3_key, local_key, verbose=verbose)
+        local_keys.append(local_key)
+    return local_keys
 
 
 def download_object(s3_client, bucket, s3_key, local_key=None, verbose=False):
+    """Downloads a single object from s3 to a local directory.
+
+    Args:
+        s3_client (obj): a low-level client of AWS S3.
+        bucket (str): the bucket name.
+        s3_key (str): the path to the s3 object along with the object name.
+        local_key (str): where the files should be downloaded to and file name.
+        verbose (bool): whether to print out a file as it is being downloaded. 
+    
+   """
+
     if not local_key:
         file_name = s3_key.split("/")[-1]
         local_key = os.path.join(".", file_name)
